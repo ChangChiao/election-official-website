42851ea399372fac9213bfabdd5f63c5115de4b3
joe <JoeCT_Chang@moxa.com>
2023-11-09 19:45:16 +0800

    feat: project init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import 'iconify-icon'
+	import '../app.css';
+	let name = 'world';
+</script>
+
+<nav>
+	<iconify-icon icon="mdi:home"></iconify-icon>
+	<h1 class="bg-blue-500 text-xl">你好我的朋友</h1>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
