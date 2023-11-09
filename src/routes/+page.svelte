0a79fea818b00b198bf192c10373816f70da0ea1
joe <JoeCT_Chang@moxa.com>
2023-11-09 22:04:18 +0800

    feat: add page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	import Amount from "../components/Amount.svelte";
+    import { onMount } from 'svelte';
 
+    let pageTitle = '喵立翰 競選官網';
+
+    onMount(() => {
+    document.title = pageTitle;
+    });
 </script>
 
 <Amount/>
