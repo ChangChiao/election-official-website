47047651408313f93c368ddd31c11c888daff7f1
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-11-11 18:42:46 +0800

    fix: text-overflow

--- src/components/IssueCard.svelte
+++ src/components/IssueCard.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+	import { string } from "zod";
+
     export let index: number = 0;
     export let order: string =''
     export let title: string = ''
-    export let content: string[] = []    
+    export let content: string[] = []
+    export let ellipsis: number = 0    
+    const getString = (str: string, count: number) => str.length >= count ? str.slice(0, count) + '...' : string;   
 </script>
 
 <div class:mt-10={index === 1} class="bg-white p-4 rounded-lg w-[30%] max-w-[374px] h-[366px]">
@@ -12,11 +16,16 @@
     <h3 class="my-2 cus-text-h4">
         {title}
     </h3>
-    <ol class="pl-4 list-decimal body-md h-[190px] overflow-hidden">
+    <ol class="pl-4 list-decimal body-md h-[190px]">
         {#each content as item, i}
-            <li  class:text-ellipsis={i === content.length - 1}>
-                {item}
+            <li >
+                {
+                    i === content.length - 1 ? (ellipsis !== 0 ? getString(item, ellipsis): item)     :item
+                }
             </li>
         {/each}
     </ol>
 </div>
+
+<style lang="postcss">
+</style>
