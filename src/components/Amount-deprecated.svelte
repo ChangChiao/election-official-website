a79b30e1c011a32bff60a7c21094a03c953b21a8
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-11-11 15:54:57 +0800

    refactor: refactor amount

---
+++ src/components/Amount-deprecated.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+let flag = false;
+let amountElemet: HTMLDivElement;
+const option = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5
+}
+
+
+
+onMount(() => {
+  const handleIntersection = ((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    entries.forEach(ele => {
+      if(ele.isIntersecting){
+        flag = true;
+        observer.unobserve(ele.target)
+      }
+    })
+  })
+  const observer = new IntersectionObserver(handleIntersection, option);
+  observer.observe(amountElemet);
+})
+
+</script>
+
+
+<div bind:this={amountElemet} class="flex h-[100px] items-center font-oswald cus-text-h1 text-primary">
+  {#if flag}  
+    <span>NT$</span> <div class="amount"></div>
+  {/if}
+</div>
+
+
+<style lang="postcss">
+   @property --num {
+  syntax: "<integer>";
+  initial-value: 0;
+  inherits: false;
+}
+
+.amount {
+  animation: counter 4s forwards ease-in-out;
+  counter-reset: num var(--num);
+  padding: 1rem 0;
+}
+.amount::after {
+  content: counter(num);
+}
+
+@keyframes counter {
+  from {
+    --num: 987655000;
+  }
+  to {
+    --num: 987655873;
+  }
+}
+
+</style>
--- src/components/Amount.svelte
+++ src/components/Amount.svelte
@@ -1,62 +1,38 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 
-let flag = false;
-let amountElemet: HTMLDivElement;
-const option = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.5
-}
+  let flag = false;
+  let amountElemet: HTMLDivElement;
+  const option = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+  }
 
 
 
-onMount(() => {
-  const handleIntersection = ((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-    entries.forEach(ele => {
-      if(ele.isIntersecting){
-        flag = true;
-        observer.unobserve(ele.target)
-      }
+  onMount(() => {
+    const counterUp = window.counterUp.default;
+    const handleIntersection = ((entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach(ele => {
+        if(ele.isIntersecting){
+          counterUp( amountElemet, {
+            duration: 1000,
+            delay: 16,
+          })
+          // flag = true;
+          observer.unobserve(ele.target)
+        }
+      })
     })
+    const observer = new IntersectionObserver(handleIntersection, option);
+    observer.observe(amountElemet);
   })
-  const observer = new IntersectionObserver(handleIntersection, option);
-  observer.observe(amountElemet);
-})
 
 </script>
 
 
 <div bind:this={amountElemet} class="flex h-[100px] items-center font-oswald cus-text-h1 text-primary">
-  {#if flag}  
-    <span>NT$</span> <div class="amount"></div>
-  {/if}
+    <span>NT$</span> <div bind:this={amountElemet} class="amount">987,655,873</div>
 </div>
 
-
-<style lang="postcss">
-   @property --num {
-  syntax: "<integer>";
-  initial-value: 0;
-  inherits: false;
-}
-
-.amount {
-  animation: counter 4s forwards ease-in-out;
-  counter-reset: num var(--num);
-  padding: 1rem 0;
-}
-.amount::after {
-  content: counter(num);
-}
-
-@keyframes counter {
-  from {
-    --num: 987655000;
-  }
-  to {
-    --num: 987655873;
-  }
-}
-
-</style>
