33644566b371923a27e55b0851caf0d5526de4c7
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-11-11 15:19:32 +0800

    refactor: refactor menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,28 +1,30 @@
-<header class="bg-white h-[100px]" >
+<script lang="ts">
+    let activeIndex = 0
+    const handleClick = (index: number) => {
+        activeIndex = index
+    }
+
+    const menu = [
+        {id: 'advocate', title: '候選人主張'},
+        {id: 'activity', title: '最新活動'},
+        {id: 'issue', title: '政策議題'},
+        {id: 'mailBox', title: '民眾服務信箱'},
+    ]
+
+</script>
+
+<header class="bg-white h-[100px] fixed w-full top-0 left-0 z-50 border-[5px] border-primary border-b-0">
     <div class="container flex items-center justify-between h-full">
         <img class="logo w-[236px] h-[58px] flex-1" src="/svg/Logo.svg" alt="logo">
         <div class="flex items-center w-2/3">
             <ul class="flex">
-                <li class="menu-item active cus-text-h5">
-                   <a href="#advocate">
-                       候選人主張
-                   </a>                   
-                </li>
-                <li class="menu-item cus-text-h5">
-                    <a href="#activity">
-                      最新活動
-                    </a>
-                </li>
-                <li class="menu-item cus-text-h5">
-                    <a href="#issue">
-                      政策議題
-                    </a>
-                </li>
-                <li class="menu-item cus-text-h5">
-                    <a href="#mailBox">
-                      民眾服務信箱
-                    </a>
-                </li>
+                {#each menu as item, i}
+                    <li class="menu-item cus-text-h5" class:active={i === activeIndex} >
+                        <a href={`#${item.id}`} on:click={() => handleClick(i)}>
+                            {item.title}
+                        </a>                   
+                    </li>
+                {/each}
             </ul>
             <button class="flex items-center px-4 py-2 text-white cursor-pointer cus-text-h5 rounded-3xl bg-primary">
                 <iconify-icon icon="mdi:attach-money"></iconify-icon>
